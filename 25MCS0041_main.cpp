33d830ccd04ec43c1b0770868ef2b9e6b6853379
Smita-18 <118966913+Smita-18@users.noreply.github.com>
2025-08-07 11:52:41 +0530

    Create 25MCS0041_main.cpp

---
+++ 25MCS0041_main.cpp
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "omp.h"
+#include "mnudrvprg_030_for_worksharing_construct.h"
+
+
+
+int main() {
+
+	
+
+
+
+	fn_mnudrvprg_030_for_worksharing_construct();
+
+
+	return 0;
+}
